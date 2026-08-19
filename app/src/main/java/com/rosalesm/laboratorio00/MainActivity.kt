84e51faae702ed210859a8e653c6f8faf7871d37
Angel Rosales Montero <angel.rosales.m@tecsup.edu.pe>
2026-08-19 16:31:17 -0500

    Proyecto Inicial del curso Programacion en moviles

---
+++ app/src/main/java/com/rosalesm/laboratorio00/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.rosalesm.laboratorio00
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.rosalesm.laboratorio00.ui.theme.Laboratorio00Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laboratorio00Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "soy Angel Rosales Montero",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = "Hola, soy $name!",
+            fontSize = 24.sp
+        )
+        Text(
+            text = "Programacion en Moviles"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laboratorio00Theme {
+        Greeting("Angel Rosales")
+    }
+}
