bd59d79d197929c46e1e91e35a8d1e67a7364937
Angel Rosales Montero <angel.rosales.m@tecsup.edu.pe>
2026-08-19 17:37:45 -0500

    Se ha agregado un boton y se ha cambiado el color predeterminado

--- app/src/main/java/com/rosalesm/laboratorio00/MainActivity.kt
+++ app/src/main/java/com/rosalesm/laboratorio00/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -42,6 +43,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         Text(
             text = "Programacion en Moviles"
         )
+        Button(onClick = {}) {
+            Text(text = "Presioname")
+        }
     }
 }
 
